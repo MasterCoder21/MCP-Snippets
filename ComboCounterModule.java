3a2767242a03c28544d5815b2449f042f156bf80
minecraftpr03 <47282021+MasterCoder21@users.noreply.github.com>
2022-05-27 05:16:09 -0700

    Create ComboCounterModule.java

---
+++ ComboCounterModule.java
@@ -0,0 +1,72 @@
+package com.streamlined.client.features.modules.type.hud;
+
+import com.streamlined.client.enums.ModuleCategory;
+import com.streamlined.client.event.SubscribeEvent;
+import com.streamlined.client.event.impl.SCAttackEntityEvent;
+import com.streamlined.client.event.impl.SCDamageEntityEvent;
+import com.streamlined.client.event.impl.SCpdateEvent;
+import com.streamlined.client.features.modules.utils.DefaultModuleRenderer;
+
+public class ComboCounterModule extends DefaultModuleRenderer {
+
+	private long hitTime = -1;
+	private int combo;
+	private int possibleTarget;
+
+	public ComboCounterModule() {
+		super("Combo Counter", "Display the number of subsequent hits.");
+	}
+	
+	@Override
+	public Object getValue() {
+		if(combo == 0) {
+			return "No combo";
+		}
+		else {
+			return combo + " hit" + ((combo > 1) ? "s" : "");
+		}
+	}
+	
+	@Override
+	public Object getDummy() {
+		return "6 hits";
+	}
+	
+	@SubscribeEvent
+	public void onUpdate(SCUpdateEvent event) {
+		if((System.currentTimeMillis() - hitTime) > 2000) {
+			combo = 0;
+		}
+	}
+
+	@SubscribeEvent
+	public void onEntityAttack(SCAttackEntityEvent event) {
+		possibleTarget = event.victim.getEntityId();
+	}
+
+	@SubscribeEvent
+	public void onEntityDamage(SCDamageEntityEvent event) {
+		if(event.entity.getEntityId() == possibleTarget) {
+			dealHit();
+		}
+		else if(event.entity == mc.thePlayer) {
+			takeHit();
+		}
+	}
+
+	public void dealHit() {
+		combo++;
+		possibleTarget = -1;
+		hitTime = System.currentTimeMillis();
+	}
+
+	public void takeHit() {
+		combo = 0;
+	}
+
+	@Override
+	public ModuleCategory getCategory() {
+		return ModuleCategory.HUD;
+	}
+
+}
